b1f80e1133a2f7209430b7b5dbf0896f85eb4838
Konrad Hendley <konradhendley@Konrads-MacBook-Pro.local>
2022-04-18 15:28:58 -0600

    new push

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-const db = require("./dbConnectExec.js")
-const thesisConfig = require("./config.js");
+const db = require("../dbConnectExec.js")
+const thesisConfig = require("../config.js");
 
 const auth = async(req,res,next)=>{
     // console.log("in the middlewere", req.header("Authorization"));
